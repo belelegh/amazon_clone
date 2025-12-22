c3854bd8a4b5395794144dd2a696c01e7524dc68
belelegh <144286852+belelegh@users.noreply.github.com>
2025-12-22 13:42:45 -0500

    cart and reducer

--- my-react-app/src/Utility/reducer.js
+++ my-react-app/src/Utility/reducer.js
@@ -9,9 +9,25 @@ export const initialState = {
 export const reducer = (state, action) => {
   switch (action.type) {
     case Type.ADD_TO_BASKET:
+      const existingItem = state.basket.find(
+        (item) => item.id === action.item.id
+      );
+      if (!existingItem) {
+        return {
+          ...state,
+          basket: [...state.basket, { ...action.item, amount: 1 }],
+        };
+      } else {
+        const updatedBasket = state.basket.map((item) => {
+          return item.id === action.item.i
+            ? { ...item, amount: item.amount + 1 }
+            : item;
+        });
+      }
+
       return {
         ...state,
-        basket: [...state.basket, action.item],
+        basket: updatedBasket,
       };
 
     default:
